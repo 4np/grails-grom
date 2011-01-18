98e4392aba223dd9dcb308baa57a6b35e855fe66
4np <work@osx.eu>
2011-01-18 14:32:54 +0000

    rewrote the Grom plugin to be less procedural, updated version to 0.2.2

--- GromGrailsPlugin.groovy
+++ GromGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import org.codehaus.groovy.grails.plugins.GrailsPluginUtils
  * $Date$
  */
 class GromGrailsPlugin {
-	def version			= "0.2.1"
+	def version			= "0.2.2"
 	def grailsVersion	= "1.3.4 > *"
 	def dependsOn		= [:]
 	def pluginExcludes	= ["grails-app/views/error.gsp"]
@@ -28,7 +28,6 @@ class GromGrailsPlugin {
 	def title			= "Grom"
 	def description		= '''Grom (Dutch for Growl) sends Grails notifications to Mac OS X and Windows using Growl, or to Linux using libnotify.'''
 	def documentation	= "http://grails.org/plugin/grom"
-	static warningSent		= false
 
 	def doWithWebDescriptor = { xml ->
 		// TODO Implement additions to web.xml (optional), this event occurs before
@@ -55,164 +54,163 @@ class GromGrailsPlugin {
 	}
 
 	def doWithDynamicMethods = { applicationContext ->
-		// make sure the application will not -in any case- break because
-		// of this plugin
+		def pluginBasePath = getPluginDirectory(application)
+		def applicationName = application.properties.metadata['app.name'].toString()
+
+		// set up Grom
 		try {
-			def pluginBasePath = getPluginDirectory(application)
-			def applicationName = application.properties.metadata['app.name'].toString()
+			setUpGrom(applicationName, pluginBasePath, log)
+		} catch (Exception e) {
+			log.error "Grom: encountered a problem setting up the global grom method"
+			log.error "  --> ${e.getMessage()}"
+		}
+	}
 
-			// check what OS we are running
-			if (System.properties["os.name"] == "Mac OS X") {
-				// It's a Mac, we can use Growl (if installed of course)
-				String.metaClass.grom = {->
-					def message = delegate.toString()
+	/**
+	 * add the grom method to the String class
+	 * @param applicationName
+	 * @param pluginBasePath
+	 * @param log
+	 * @return
+	 */
+	static setUpGrom(String applicationName, String pluginBasePath, log) {
+		try {
+			// try to send a message
+			grom('Starting application', applicationName, pluginBasePath)
 
-					try {
-						// use a custom Apple script to send Growl notifications
-						def cmd = [
-							"osascript",
-							"${pluginBasePath}/web-app/lib/grom.scpt",
-							applicationName,
-							message
-						]
-
-						Runtime.getRuntime().exec((String[]) cmd.toArray())
-					} catch (Exception e) {
-						// no gromming for now?
-						if (!warningSent) {
-							warningSent = true
-							log.error "Grom: plugin encountered a problem sending growl notifications"
-							log.error "      --> ${e.getMessage()}"
-						}
-
-						log.debug "Grom: ${message}"
-					}
-				}
-			} else if (System.properties["os.name"] == "Linux") {
-				// it's time to notify tux
-				//
-				// Unfortunately the timeout parameter (-t) is broken in
-				// Ubuntu so the 2 second timeout is being ignored. Also,
-				// the installation in Ubuntu does not allow customization
-				// of notifications. However, install the leolik patch in
-				// Ubuntu to resolve both issues:
-				// http://www.webupd8.org/2010/05/finally-easy-way-to-customize-notify.html
-				//
-				// Also, appending messages does not seem to work when using
-				// notify-bin. Put it in here if this feature will be implemented
-				// in the future. Also see:
-				// https://bugs.launchpad.net/ubuntu/+source/notify-osd/+bug/434913
-				String.metaClass.grom = {->
-					def message = delegate.toString()
-
-					try {
-						def cmd = [
-							"notify-send",
-							"-t",
-							"2000",
-							"-u",
-							"critical",
-							"-h",
-							"string:x-canonical-append:allowed",
-							"--icon",
-							"${pluginBasePath}/web-app/lib/grailslogo.png",
-							"Grails :: ${applicationName}",
-							"${message}"
-						]
-
-						Runtime.getRuntime().exec((String[]) cmd.toArray())
-					} catch (Exception e) {
-						// check if libnotify is installed
-						if (e.getMessage() =~ "No such file") {
-							if (!warningSent) {
-								warningSent = true
-								log.error "Grom: please install libnotify in order to receive notifications from the Grom plugin"
-								log.error "      see ${documentation} for more information"
-							}
-
-							log.debug "Grom: ${message}"
-						} else {
-							if (!warningSent) {
-								warningSent = true
-								log.error "Grom: plugin encounted a problem sending notifications"
-								log.error "      --> ${e.getMessage()}"
-							}
-
-							log.debug "Grom: ${message}"
-						}
-					}
-				}
-			} else if (System.properties["os.name"] =~ "Windows") {
-				// use growl for windows to send notifications
-				// Note: growlnotify requires the Microsoft .NET Framework (v2.0 or higher)
-				String.metaClass.grom = {->
-					def message = delegate.toString()
-					try {
-						def cmd = [
-							"\"${pluginBasePath}\\web-app\\lib\\growlnotify.com\"",
-							"/a:\"${applicationName}\"",
-							"/r:\"Grom\"",
-							"/n:\"Grom\"",
-							"/t:\"Grails :: ${applicationName}\"",
-							"/i:\"${pluginBasePath}\\web-app\\lib\\grailslogo.png\"",
-							"\"${message}\""
-						]
-
-						// execute command. Use String.execute as runtime.exec does
-						// not work properly in combination with growlnotify.com
-						def process = cmd.join(' ').execute()
-						def feedback = ""
-
-						// handle feedback
-						process.in.eachLine { line -> feedback += line }
-						if (feedback =~ "The destination server was not reachable") {
-							if (!warningSent) {
-								warningSent = true
-								log.error "Grom: please install growl for windows in order to receive notifications from the Grom plugin"
-								log.error "      see ${documentation} for more information"
-							}
-
-							log.debug "Grom: ${message}"
-						} else if (!(feedback =~ "Notification sent successfully")) {
-							throw new Exception(feedback)
-						}
-					} catch (Exception e) {
-						// no gromming for now?
-						if (!warningSent) {
-							warningSent = true
-							log.error "Grom: plugin encountered a problem sending growl notifications"
-							log.error "      --> ${e.getMessage()}"
-						}
-
-						log.debug "Grom: ${message}"
-					}
-				}
-			} else {
-				// unsupported OS
-				String.metaClass.grom = {
-					if (!warningSent) {
-						warningSent = true
-						log.error "Grom: ${System.properties["os.name"]} is currently not supported"
-						log.error "      see ${documentation} for more information"
-					}
-
-					log.debug "Grom: ${message}"
+			// all went well, set up grom method
+			String.metaClass.grom = { ->
+				def message = delegate.toString()
+				try {
+					grom(message, applicationName, pluginBasePath)
+				} catch (Exception e) {
+					log.error "Grom: encountered an error gromming a message (${message})"
+					log.error "  --> ${e.getMessage()}"
 				}
 			}
 		} catch (Exception e) {
-			// something went terribly wrong... :S
-			// show feedback...
-			if (!warningSent) {
-				warningSent = true
-				log.error "Grom: plugin has encountered a problem setting up the global grom method"
-				log.error "      --> ${e.getMessage()}"
-			}
+			// make sure the grom method is available...
+			String.metaClass.grom = { }
 
-			// ...and make sure the method is available
-			String.metaClass.grom = {
-				// just print it to the terminal
-				log.debug "Grom: ${delegate.toString()}"
+			// ...and pass on the exception
+			throw new Exception(e.getMessage())
+		}
+	}
+
+	/**
+	 * Send a message to the Window Manager's unified messaging system
+	 * @param message
+	 * @param applicationName
+	 * @param pluginBasePath
+	 * @return
+	 */
+	static grom(String message, String applicationName, String pluginBasePath) {
+		if (System.properties["os.name"] == "Mac OS X") {
+			gromMacOS(message, applicationName, pluginBasePath)
+		} else if (System.properties["os.name"] == "Linux") {
+			gromLinux(message, applicationName, pluginBasePath)
+		} else if (System.properties["os.name"] =~ "Windows") {
+			gromWindows(message, applicationName, pluginBasePath)
+		} else {
+			throw new Exception("unsupported OS ${System.properties["os.name"]}")
+		}
+	}
+
+	/**
+	 * Send a message to Mac OS X using Growl
+	 * @param message
+	 * @param applicationName
+	 * @param pluginBasePath
+	 * @return
+	 */
+	static gromMacOS(String message, String applicationName, String pluginBasePath) {
+		// use a custom Apple script to send Growl notifications
+		def cmd = [
+			"osascript",
+			"${pluginBasePath}/web-app/lib/grom.scpt",
+			applicationName,
+			message
+		]
+
+		Runtime.getRuntime().exec((String[]) cmd.toArray())
+	}
+
+	/**
+	 * send a message to Linux using libnotify
+	 * @param message
+	 * @param applicationName
+	 * @param pluginBasePath
+	 * @return
+	 */
+	static gromLinux(String message, String applicationName, String pluginBasePath) {
+		// it's time to notify tux
+		//
+		// Unfortunately the timeout parameter (-t) is broken in
+		// Ubuntu so the 2 second timeout is being ignored. Also,
+		// the installation in Ubuntu does not allow customization
+		// of notifications. However, install the leolik patch in
+		// Ubuntu to resolve both issues:
+		// http://www.webupd8.org/2010/05/finally-easy-way-to-customize-notify.html
+		//
+		// Also, appending messages does not seem to work when using
+		// notify-bin. Put it in here if this feature will be implemented
+		// in the future. Also see:
+		// https://bugs.launchpad.net/ubuntu/+source/notify-osd/+bug/434913
+		try {
+			def cmd = [
+				"notify-send",
+				"-t",
+				"2000",
+				"-u",
+				"critical",
+				"-h",
+				"string:x-canonical-append:allowed",
+				"--icon",
+				"${pluginBasePath}/web-app/lib/grailslogo.png",
+				"Grails :: ${applicationName}",
+				"${message}"
+			]
+
+			Runtime.getRuntime().exec((String[]) cmd.toArray())
+		} catch (Exception e) {
+			// check if libnotify is installed
+			if (e.getMessage() =~ "No such file") {
+				throw new Exception("libnotify is not installed")
+			} else {
+				throw new Exception(e.getMessage())
 			}
+		}
+	}
+
+	/**
+	 * Send a message to Windows using Growl for Windows
+	 * @param message
+	 * @param applicationName
+	 * @param pluginBasePath
+	 * @return
+	 */
+	static gromWindows(String message, String applicationName, String pluginBasePath) {
+		def cmd = [
+			"\"${pluginBasePath}\\web-app\\lib\\growlnotify.com\"",
+			"/a:\"${applicationName}\"",
+			"/r:\"Grom\"",
+			"/n:\"Grom\"",
+			"/t:\"Grails :: ${applicationName}\"",
+			"/i:\"${pluginBasePath}\\web-app\\lib\\grailslogo.png\"",
+			"\"${message}\""
+		]
+
+		// execute command. Use String.execute as runtime.exec does
+		// not work properly in combination with growlnotify.com
+		def process = cmd.join(' ').execute()
+		def feedback = ""
+
+		// handle feedback
+		process.in.eachLine { line -> feedback += line }
+		if (feedback =~ "The destination server was not reachable") {
+			throw new Exception("growl for windows is not installed or not accepting messages")
+		} else if (!(feedback =~ "Notification sent successfully")) {
+			throw new Exception(feedback)
 		}
 	}
 
